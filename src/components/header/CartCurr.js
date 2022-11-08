1e46f4bac922d122553e6ef132b9c9ead469298c
sapientiam-congnitio <spatium-memoriae@protonmail.com>
2022-11-08 14:18:19 +0600

    Final

---
+++ src/components/header/CartCurr.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Currency from "./Currency";
+import { CartAndCurrency } from "./styles/style-nav";
+import CartModal from "../cart/cart-overlay";
+
+export default class CartCurr extends Component {
+
+  render() {
+    return (
+      <>
+        <CartAndCurrency>
+          <Currency />
+          <CartModal />
+        </CartAndCurrency>
+      </>
+    );
+  }
+}
